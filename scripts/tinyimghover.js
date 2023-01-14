fc3c49261d67e5b1238e1a365bb438b4361d1b19
misternooblet <dragonroadsupport@gmail.com>
2023-01-14 17:35:30 +0200

    zoombeta

--- scripts/tinyimghover.js
+++ scripts/tinyimghover.js
@@ -1,18 +1,40 @@
 const tinyimg1 = document.getElementById("tinyimg1");
 const tinyimg2 = document.getElementById("tinyimg2");
 const tinyimg3 = document.getElementById("tinyimg3");
-
 tinyimg1.addEventListener("mouseover", function () {
   document.getElementById("bigimg").style.background =
     'url("/assets/images/picklejarbox.jpg") center center/contain no-repeat';
+  document.getElementById("hugepic").style.background =
+    'url("/assets/images/picklejarbox.jpg")';
+  document.getElementById("hugepic").style.backgroundRepeat = "no-repeat";
+  document.getElementById("hugepic").style.backgroundSize = "contain";
+  document.getElementById("hugepic").style.width = "200%";
+  document.getElementById("hugepic").style.height = "200%";
+  document.getElementById("hugepic").style.position = "absolute";
 });
 
 tinyimg2.addEventListener("mouseover", function () {
   document.getElementById("bigimg").style.background =
     'url("/assets/images/picklejar.jpg") center center/contain no-repeat';
+  document.getElementById("hugepic").style.background =
+    'url("/assets/images/picklejar.jpg")';
+  document.getElementById("hugepic").style.backgroundRepeat = "no-repeat";
+  document.getElementById("hugepic").style.backgroundSize = "contain";
+  document.getElementById("hugepic").style.width = "200%";
+  document.getElementById("hugepic").style.height = "200%";
+  document.getElementById("hugepic").style.position = "absolute";
+  document.getElementById("hugepic").style.backgroundPosition = "center";
 });
 
 tinyimg3.addEventListener("mouseover", function () {
   document.getElementById("bigimg").style.background =
     'url("/assets/images/picklebox.jpg") center center/contain no-repeat';
+  document.getElementById("hugepic").style.background =
+    'url("/assets/images/picklebox.jpg")';
+  document.getElementById("hugepic").style.backgroundRepeat = "no-repeat";
+  document.getElementById("hugepic").style.backgroundSize = "contain;";
+  document.getElementById("hugepic").style.width = "200%";
+  document.getElementById("hugepic").style.height = "200%";
+  document.getElementById("hugepic").style.position = "absolute";
+  document.getElementById("hugepic").style.backgroundPosition = "center";
 });
---
+++ scripts/zoom.js
@@ -0,0 +1,32 @@
+const bigimg = document.getElementById("bigimg");
+
+bigimg.addEventListener("mouseover", function () {
+  document.getElementById("zoombox").style.display = "inline";
+  document.getElementById("zoombox").style.display = "inline";
+});
+bigimg.addEventListener("mouseleave", function () {
+  document.getElementById("zoombox").style.display = "none";
+});
+
+bigimg.addEventListener("mousemove", function (event) {
+  let rect = bigimg.getBoundingClientRect();
+
+  let heightPercent = (
+    ((event.clientY - rect.top) / rect.height) *
+    100
+  ).toFixed();
+  let widthPercent = (
+    ((event.clientX - rect.left) / rect.width) *
+    100
+  ).toFixed();
+
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+
+  document.getElementById("hugepic").style.top = `-${heightPercent}%`;
+  document.getElementById("hugepic").style.left = `-${widthPercent}%`;
+
+  console.log("Cursor position: " + x + "," + y);
+  console.log("Cursor percents: " + widthPercent + "," + heightPercent);
+  console.log(rect.width);
+});
