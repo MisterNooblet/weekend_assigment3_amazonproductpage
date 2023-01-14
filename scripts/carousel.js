dce7cf47fdd2f274df1c1425be7906fb44e5823e
misternooblet <dragonroadsupport@gmail.com>
2023-01-14 14:38:12 +0200

    scripts to bottm

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,33 +1,31 @@
-window.onload = function () {
-  const forward = document.getElementById("nextpage");
-  const back = document.getElementById("previouspage");
+const forward = document.getElementById("nextpage");
+const back = document.getElementById("previouspage");
 
-  let currentPage = 1;
+let currentPage = 1;
 
-  forward.addEventListener("click", function () {
-    currentPage = 2;
-    document.getElementById("pagecount").textContent = currentPage;
-    for (let element of document.getElementsByClassName("pg1")) {
-      element.style.display = "none";
-    }
-    for (let element of document.getElementsByClassName("pg2")) {
-      element.style.display = "inline";
-    }
-    document.getElementById("startover").style.display = "inline";
-  });
+forward.addEventListener("click", function () {
+  currentPage = 2;
+  document.getElementById("pagecount").textContent = currentPage;
+  for (let element of document.getElementsByClassName("pg1")) {
+    element.style.display = "none";
+  }
+  for (let element of document.getElementsByClassName("pg2")) {
+    element.style.display = "inline";
+  }
+  document.getElementById("startover").style.display = "inline";
+});
 
-  back.addEventListener("click", function () {
-    currentPage = 1;
-    document.getElementById("pagecount").textContent = currentPage;
-    for (let element of document.getElementsByClassName("pg1")) {
-      element.style.display = "inline";
-    }
-    for (let element of document.getElementsByClassName("pg2")) {
-      element.style.display = "none";
-    }
-    document.getElementById("startover").style.display = "none";
-  });
-};
+back.addEventListener("click", function () {
+  currentPage = 1;
+  document.getElementById("pagecount").textContent = currentPage;
+  for (let element of document.getElementsByClassName("pg1")) {
+    element.style.display = "inline";
+  }
+  for (let element of document.getElementsByClassName("pg2")) {
+    element.style.display = "none";
+  }
+  document.getElementById("startover").style.display = "none";
+});
 
 function resetcarousel() {
   currentPage = 1;
--- scripts/tinyimghover.js
+++ scripts/tinyimghover.js
@@ -1,20 +1,18 @@
-window.onload = function () {
-  const tinyimg1 = document.getElementById("tinyimg1");
-  const tinyimg2 = document.getElementById("tinyimg2");
-  const tinyimg3 = document.getElementById("tinyimg3");
+const tinyimg1 = document.getElementById("tinyimg1");
+const tinyimg2 = document.getElementById("tinyimg2");
+const tinyimg3 = document.getElementById("tinyimg3");
 
-  tinyimg1.addEventListener("mouseover", function () {
-    document.getElementById("bigimg").style.background =
-      'url("/assets/images/picklejarbox.jpg") center center/contain no-repeat';
-  });
+tinyimg1.addEventListener("mouseover", function () {
+  document.getElementById("bigimg").style.background =
+    'url("/assets/images/picklejarbox.jpg") center center/contain no-repeat';
+});
 
-  tinyimg2.addEventListener("mouseover", function () {
-    document.getElementById("bigimg").style.background =
-      'url("/assets/images/picklejar.jpg") center center/contain no-repeat';
-  });
+tinyimg2.addEventListener("mouseover", function () {
+  document.getElementById("bigimg").style.background =
+    'url("/assets/images/picklejar.jpg") center center/contain no-repeat';
+});
 
-  tinyimg3.addEventListener("mouseover", function () {
-    document.getElementById("bigimg").style.background =
-      'url("/assets/images/picklebox.jpg") center center/contain no-repeat';
-  });
-};
+tinyimg3.addEventListener("mouseover", function () {
+  document.getElementById("bigimg").style.background =
+    'url("/assets/images/picklebox.jpg") center center/contain no-repeat';
+});
